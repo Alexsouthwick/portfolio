00cc8362707c884a5ef8be2a844d038e9fbde329
Alexandra Southwick <acsouthwick@gmail.com>
2020-01-17 22:40:03 -0500

    who even knows

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
-
 function hover(){
-	var gwc = document.getElementbyId("gwc");
-	gwc.classList.toggle("hover");
+	var gwc = document.getElementsByClassName("gwc");
+	gwc[0].className = "hover";
 	}
+
+function normal(){
+	var hover = document.getElementsByClassName("hover");
+	hover[0].className = "gwc";
+}
+
+
+$(document).ready(function() {
+
+var navBar = document.getElementById("nav");
+var tabs = navBar.getElementsByClassName("tab");
+
+for(var i=0; i<tabs.length; i++){
+	tabs[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
+}
+
+});
